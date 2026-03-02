1722d76e6359b7f36f291b22adab4d665af55d55
rawli <raulgirardi34@gmail.com>
2026-03-01 21:31:17 -0300

    almost done i think

--- src/game/planet_manager.cpp
+++ src/game/planet_manager.cpp
@@ -1,7 +1,45 @@
 #include "planet_manager.h"
 #include "renderer.h"
 #include "physics.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <vector>
-#include <iostream>
+#include <memory>
 
 //TODO: working objects manager
+gameObject::gameObject(physicsArgs pArgs, SHADER_TYPE type, glm::vec3 color, float radius): 
+position(pArgs.position),
+velocity(pArgs.velocity), 
+mass(pArgs.mass),
+type(type),
+color(color),
+radius(radius),
+physics(position, velocity, mass, pArgs.density),
+render(type, radius, color) {}
+
+void gameObject::addStar(physicsArgs pArgs, glm::vec3 color, float radius) {
+    stars.push_back(std::make_unique<gameObject>(pArgs, SHADER_TYPE::STAR, color, radius));
+}
+
+void gameObject::addPlanet(physicsArgs pArgs, glm::vec3 color, float radius) {
+    planets.push_back(std::make_unique<gameObject>(pArgs, SHADER_TYPE::PLANET, color, radius));
+}
+
+void gameObject::drawStars(const glm::mat4 &view, const glm::mat4 &projection) {
+    for(auto& star : stars) {
+        glm::mat4 model = glm::translate(glm::mat4(0.0f), star->position);
+        star->render.draw(star->position, model, view, projection);
+    }
+}
+
+void gameObject::drawPlanets(const glm::mat4 &view, const glm::mat4 &projection) {
+    for(auto& planet : planets) {
+        glm::mat4 model = glm::translate(glm::mat4(0.0f), planet->position);
+        planet->render.draw(planet->position, model, view, projection);
+    }
+}
+
+float gameObject::getMass() { return mass; }
+float gameObject::getRadius() { return radius; }
+
+glm::vec3 gameObject::getPosition() { return position; }
--- src/game/planet_manager.h
+++ src/game/planet_manager.h
@@ -1,7 +1,52 @@
 #pragma once
 #include "renderer.h"
 #include "physics.h"
+#include <glm/glm.hpp>
 #include <vector>
-#include <iostream>
+#include <memory>
 
 //TODO: working objects manager
+struct physicsArgs {
+    glm::vec3 position;
+    glm::vec3 velocity;
+
+    float mass;
+    float density;
+};
+
+class gameObject {
+public:
+    static void addStar(physicsArgs pArgs, glm::vec3 color, float radius);
+    static void addPlanet(physicsArgs pArgs, glm::vec3 color, float radius);
+
+    static void drawStars(const glm::mat4 &view, const glm::mat4 &projection);
+    static void drawPlanets(const glm::mat4 &view, const glm::mat4 &projection);
+
+    float getMass();
+    float getRadius();
+
+    glm::vec3 getPosition();
+private:
+    physicsObject physics;
+    renderObject render;
+
+    gameObject(physicsArgs pArgs, SHADER_TYPE type, glm::vec3 color, float radius);
+    
+    static std::vector<std::unique_ptr<gameObject>> stars;
+    static std::vector<std::unique_ptr<gameObject>> planets;
+
+    //physics
+    const float mass;
+
+    glm::vec3 position;
+    glm::vec3 velocity;
+    glm::vec3 acceleration;
+
+    //render
+    SHADER_TYPE type;
+
+    const glm::vec3 color;
+
+    //both
+    const float radius;
+};
