60a74462ace3608b127f760dbaa06c76cee8785b
rawli <raulgirardi34@gmail.com>
2026-03-03 18:44:37 -0300

    added a function to calculate how the planets will attract each other based on the euler's method

--- src/engine/physics.cpp
+++ src/engine/physics.cpp
@@ -11,6 +11,19 @@ position(position), velocity(velocity), acceleration(0.0f), mass(mass) {
     this->radius = std::cbrt((3.0f * volume) / (4.0f * std::numbers::pi_v<float>));
 }
 
+glm::vec3 physicsObject::calculateGravity(const physicsObject& obj1, const physicsObject& obj2) {
+    glm::vec3 direction = obj1.position - obj2.position;
+    float distance = glm::length(direction);
+    
+    if(distance <= 0.01f) { return glm::vec3(0.0f); }
+    
+    float forceMagnitude = (GRAVITY * obj1.mass * obj2.mass) / (distance * distance);
+    glm::vec3 force = glm::normalize(direction) * forceMagnitude;
+    
+    return force;
+}
+
+
 void physicsObject::applyForce(const glm::vec3 &force) {
     this->acceleration += force / this->mass;
 }
--- src/engine/physics.h
+++ src/engine/physics.h
@@ -3,15 +3,18 @@
 
 class physicsObject {
 public:
+    static constexpr float GRAVITY = 100.0f;
+
     physicsObject(glm::vec3 position, glm::vec3 velocity, float mass, float density);
 
+    static glm::vec3 calculateGravity(const physicsObject& obj1, const physicsObject& obj2);
     void applyForce(const glm::vec3 &force);
     void applyVelocity(float deltaTime);
 
     float getMass();
     float getRadius();
 
-    glm::vec3 getPosition ();
+    glm::vec3 getPosition();
 private:
     float mass;
     float radius;
