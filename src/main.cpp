6e13d494842e8f11fd28964f706e44f3adc13622
rawli <raulgirardi34@gmail.com>
2026-03-06 21:34:39 -0300

    fixed a bug with the depth buffer bit, added another entity and updated the draw function

--- src/main.cpp
+++ src/main.cpp
@@ -19,21 +19,22 @@ int main() {
     camera::speed = 75.0f;
     camera::processCursorCallback(window.getWindow());
 
-    entityManager::addPlanet({glm::vec3(0.0f), glm::vec3(0.0f), 5000.0f, 200.0f}, glm::vec3(1.0f));
+    entityManager::addStar({glm::vec3(0.0f), glm::vec3(-10.0f, 0.0f, 0.0f), 50.0f, 5.0f}, glm::vec3(1.0f, 0.0f, 0.0f));
+    entityManager::addStar({glm::vec3(0.0f, 0.0f, -25.0f), glm::vec3(10.0f, 0.0f, 0.0f), 50.0f, 5.0f}, glm::vec3(1.0f, 1.0f, 0.0f));
 
     while(!window.windowShouldClose()) {
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         float deltaTime = getDeltaTime();
 
         camera::processKeyboardInput(window.getWindow(), deltaTime);
 
         glm::mat4 view = camera::getViewMatrix();
-        glm::mat4 projection = camera::getProjectionMatrix(55.0f, window.getWidth(), window.getHeight(), 0.01f, 750.0f);
+        glm::mat4 projection = camera::getProjectionMatrix(55.0f, window.getWidth(), window.getHeight(), 0.01f, 2500.0f);
 
         entityManager::updatePhysics(deltaTime);
-        entityManager::drawPlanets(view, projection);
+        entityManager::drawStars(view, projection);
 
         window.swapBuffers();
         win::pollEvents();
