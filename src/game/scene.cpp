72b207f5bc0dc06787b7adca8482d95e2152ba80
rawli <raulgirardi34@gmail.com>
2026-03-20 21:33:13 -0300

    added support for the new Controller class

--- src/game/scene.cpp
+++ src/game/scene.cpp
@@ -1,15 +1,13 @@
 #include "game/scene.h"
+#include "game/controller.h"
 #include "game/entity_manager.h"
 #include "engine/grid_renderer.h"
-#include "engine/camera.h"
 #include "engine/json_reader.h"
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
-#include <vector>
-#include <iostream>
 
-std::unique_ptr<win> Scene::window = nullptr;
+std::unique_ptr<Window> Scene::window = nullptr;
 std::vector<float> Scene::windowColor;
 
 void Scene::createEntities(const std::string& jsonPath) {
@@ -60,13 +58,16 @@ void Scene::init(const std::string& ScenePath) {
     JsonFile physicsConfig("game/config/physics_config.json");
     JsonFile windowConfig("game/config/window_config.json");
 
+    Controller::cameraSpeed = cameraConfig.get<float>("speed");
+    Controller::cameraScrollSpeed = cameraConfig.get<float>("scroll_speed");
+
     std::vector<float> CameraPos = cameraConfig.get<std::vector<float>>("initial_position");
-    Camera::setInitialPosition(glm::vec3(CameraPos[0], CameraPos[1], CameraPos[2]));
-    Camera::speed = cameraConfig.get<float>("speed");
-    Camera::scrollSpeed = cameraConfig.get<float>("scroll_speed");
-    Camera::fov = cameraConfig.get<float>("fov");
-    Camera::nearPlane = cameraConfig.get<float>("near_plane");
-    Camera::farPlane = cameraConfig.get<float>("far_plane");
+    Controller::setCameraPosition(glm::vec3(CameraPos[0], CameraPos[1], CameraPos[2]));
+
+    Controller::cameraFov = cameraConfig.get<float>("fov");
+    Controller::cameraSensitivity = cameraConfig.get<float>("sensitivity");
+    Controller::cameraNearPlane = cameraConfig.get<float>("near_plane");
+    Controller::cameraFarPlane = cameraConfig.get<float>("far_plane");
 
     std::vector<float> ambientValue = graphicConfig.get<std::vector<float>>("ambient");
     SphereRenderer::ambient = glm::vec3(ambientValue[0], ambientValue[1], ambientValue[2]);
@@ -74,14 +75,14 @@ void Scene::init(const std::string& ScenePath) {
     SphereRenderer::diffuse = glm::vec3(diffuseValue[0], diffuseValue[1], diffuseValue[2]);
 
     gridConfig::SOFTENING = gridConfig.get<float>("softening");
-    gridConfig::INTESITY = gridConfig.get<float>("intesity");
+    gridConfig::INTESITY = gridConfig.get<float>("intensity");
     gridConfig::DECAY = gridConfig.get<float>("decay");
 
     physicsConstants::GRAVITY = physicsConfig.get<float>("gravity");
     physicsConstants::MIN_DISTANCE = physicsConfig.get<float>("min_distance");
     physicsConstants::MIN_MASS = physicsConfig.get<float>("min_mass");
 
-    window = std::make_unique<win>(
+    window = std::make_unique<Window>(
         windowConfig.get<int>("window_width"),
         windowConfig.get<int>("window_height"), 
         windowConfig.get<std::string>("title")
@@ -92,6 +93,7 @@ void Scene::init(const std::string& ScenePath) {
     createEntities(ScenePath);
 
     Grid::init(gridConfig.get<int>("size"), gridConfig.get<float>("step"));
+    Controller::init(window->getWindow());
 }
 
 void Scene::run() {    
@@ -99,25 +101,25 @@ void Scene::run() {
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-    Camera::processCursorCallback(window->getWindow());
-    Camera::processScrollCallback(window->getWindow());
+    Controller::moveCameraScroll();
 
     while(!window->windowShouldClose()) {
         glClearColor(windowColor[0], windowColor[1], windowColor[2], windowColor[3]);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         float deltaTime = getDeltaTime();
 
-        glm::mat4 view = Camera::getViewMatrix();
-        glm::mat4 projection = Camera::getProjectionMatrix(window->getWidth(), window->getHeight());
+        glm::mat4 view = Controller::getViewMatrix();
+        glm::mat4 projection = Controller::getProjectionMatrix(window->getWidth(), window->getHeight());
 
-        Camera::processKeyboardInput(window->getWindow(), deltaTime);
+        Controller::moveCameraKeyboard(deltaTime);
+        Controller::moveCameraAngle();
 
         EntityManager::updatePhysics(deltaTime);
 
         std::vector<float> masses = EntityManager::getMasses();
         std::vector<glm::vec3> positions  = EntityManager::getPositions();
 
-        EntityManager::drawStars(Camera::getPosition(), view, projection);
+        EntityManager::drawStars(Controller::getCameraPosition(), view, projection);
         EntityManager::drawPlanets(view, projection);
 
         std::vector<objectsData> entities;
@@ -126,8 +128,9 @@ void Scene::run() {
         }
         Grid::draw(entities, view, projection);
 
+        Controller::reset();
         window->swapBuffers();
-        win::pollEvents();
+        Window::pollEvents();
     }
 }
 
@@ -135,5 +138,5 @@ void Scene::terminate() {
     Grid::terminate();
     EntityManager::terminate();
 
-    win::terminate();
+    Window::terminate();
 }
